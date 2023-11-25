51d9022bce10f67dc0f65b80e8fe9b3c7add1492
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2023-11-25 13:32:11 -0500

    Base pickup class

---
+++ Source/Unforgotten/Private/Pickups/Pickup.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickups/Pickup.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+APickup::APickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	OverlapSphere = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	OverlapSphere->SetupAttachment(RootComponent);
+	OverlapSphere->SetSphereRadius(150.f);
+	OverlapSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	OverlapSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	PickupMesh->SetupAttachment(OverlapSphere);
+	PickupMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+}
+
+// Called every frame
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult) 
+{
+
+}
+
+void APickup::Destroyed() 
+{
+	Super::Destroyed();
+
+	if (PickupSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(
+			this,
+			PickupSound,
+			GetActorLocation()
+		);
+	}
+}
+
---
+++ Source/Unforgotten/Public/Pickups/Pickup.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class UNFORGOTTEN_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Destroyed() override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* OverlapSphere;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* PickupSound;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+
+public:	
+
+
+};
